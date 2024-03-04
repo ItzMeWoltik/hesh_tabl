58b9c214e2b82469fbf81526eaa23aa546edfb82
ItzMeWoltik <itzmewoltik@gmail.com>
2024-03-05 00:47:11 +0200

    ну тіпа

---
+++ hesh_tabl/HeshTable.cs
@@ -0,0 +1,34 @@
+﻿public class HashTable
+{
+    private Dictionary<byte, string> table;
+
+    public HashTable()
+    {
+        table = new Dictionary<byte, string>();
+    }
+
+    public byte Add(string word)
+    {
+        byte hash = CalculateHash(word);
+        table[hash] = word;
+        return hash;
+    }
+
+    public string Get(byte hash)
+    {
+        if (table.ContainsKey(hash))
+            return table[hash];
+        else
+            return null;
+    }
+
+    private byte CalculateHash(string word)
+    {
+        byte hash = 0;
+        foreach (char c in word)
+        {
+            hash += (byte)c;
+        }
+        return hash;
+    }
+}
---
+++ hesh_tabl/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashTable hashTable = new HashTable();
+
+        byte hash1 = hashTable.Add("apple");
+        byte hash2 = hashTable.Add("banana");
+        byte hash3 = hashTable.Add("orange");
+
+        Console.WriteLine("Hash for 'apple': " + hash1);
+        Console.WriteLine("Hash for 'banana': " + hash2);
+        Console.WriteLine("Hash for 'orange': " + hash3);
+
+        string word1 = hashTable.Get(hash1);
+        string word2 = hashTable.Get(hash2);
+        string word3 = hashTable.Get(hash3);
+
+        Console.WriteLine("\nWord for hash " + hash1 + ": " + word1);
+        Console.WriteLine("Word for hash " + hash2 + ": " + word2);
+        Console.WriteLine("Word for hash " + hash3 + ": " + word3);
+
+        Console.ReadLine();
+    }
+}
